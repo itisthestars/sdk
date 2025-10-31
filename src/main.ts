8a916d935dcd5f574c08ef2d554e2ac0b6e2400a
itisthestars <2524569063@qq.com>
2025-10-31 14:21:27 +0800

    1\

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ErrorStackParser from 'error-stack-parser'
 import App from './App.vue'
 import router from './router'
 
@@ -10,9 +10,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.mount('#app')
 app.config.errorHandler = function (err, vm) {
     // handle error
-    console.error('Global Error Handler:', err, vm)
+    // console.error('Global Error Handler:', err, vm)
+    const stackFrames = ErrorStackParser.parse(err as Error)
+    console.log('Parsed Stack Frames:', stackFrames)
 }
+app.mount('#app')
