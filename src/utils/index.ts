0d2bc4cbd90c1dac9bd1401d615304b8d60cc731
itisthestars <2524569063@qq.com>
2025-11-06 10:12:48 +0800

    提交

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,7 +13,8 @@ const findCodeBySourceMap = async (stackFrame: any) => {
   // 通过报错位置查找到对应的源文件名称以及报错行数
   const originalPosition = consumer.originalPositionFor({
     line: stackFrame.lineNumber,
-    column: stackFrame.columnNumber
+    column: stackFrame.columnNumber 
+    
   })
   // 那么就可以通过 sourceContentFor 这个方法找到报错的源代码
   const code = consumer.sourceContentFor(originalPosition.source)
